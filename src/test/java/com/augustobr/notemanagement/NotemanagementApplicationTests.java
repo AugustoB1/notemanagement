624fde182d744fed24ef27d9739ae0bfcce4f2b8
AugustoB1 <guto.brito280802@gmail.com>
2023-07-23 20:30:51 -0300

    Project created

---
+++ src/test/java/com/augustobr/notemanagement/NotemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustobr.notemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
